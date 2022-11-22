f0a36eec6ae6d14438e02d93126dcb6127ab9dc0
trang21-uet <trang21.uet@gmail.com>
2022-11-22 20:43:52 +0700

    Home

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { StatusBar, Platform, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Settings, MainScreen, Login, Register } from './src';
@@ -19,17 +20,24 @@ const theme = {
 
 export default function App() {
   return (
-    <NavigationContainer theme={theme}>
-      <Stack.Navigator
-        initialRouteName='Home'
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name='MainScreen' component={MainScreen} />
-        <Stack.Screen name='Settings' component={Settings} />
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Register' component={Register} />
-        <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View
+      style={{
+        flex: 1,
+        marginTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0,
+      }}
+    >
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator
+          initialRouteName='Home'
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name='MainScreen' component={MainScreen} />
+          <Stack.Screen name='Settings' component={Settings} />
+          <Stack.Screen name='Login' component={Login} />
+          <Stack.Screen name='Register' component={Register} />
+          <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,57 +1,134 @@
-import { StyleSheet, Text, View, TouchableNativeFeedback } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { MyButton } from '../../components';
+import { useNavigation, useTheme } from '@react-navigation/native';
+import { MyIcon, MyInput } from '../../components';
 
 export default function Home() {
-  const navigation = useNavigation();
+  const { colors } = useTheme();
+  const styles = getStyles(colors);
   return (
     <View style={styles.container}>
-      <MyButton
-        title='Home'
-        onPress={() => navigation.navigate('Home')}
-        style={styles.button}
-        textStyle={styles.text}
-      />
-      <MyButton
-        title='Search'
-        onPress={() => navigation.navigate('Search')}
-        style={styles.button}
-        textStyle={styles.text}
-      />
-      <MyButton
-        title='Register'
-        onPress={() => navigation.navigate('Register')}
-        style={styles.button}
-        textStyle={styles.text}
-      />
-      <MyButton
-        title='Login'
-        onPress={() => navigation.navigate('Login')}
-        style={styles.button}
-        textStyle={styles.text}
-      />
+      <View style={styles.banner}>
+        <View style={styles.header}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Foodster</Text>
+          <Text style={{ fontSize: 16 }}>Hanoi</Text>
+        </View>
+      </View>
+
+      <View style={styles.search}>
+        <MyInput
+          style={styles.searchBox}
+          placeholder='Tìm kiếm món ăn, nhà hàng...'
+        />
+        <TouchableOpacity style={styles.searchBtn}>
+          <MyIcon name='search' size={25} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+        <FoodCategory name='Pizza' />
+        <FoodCategory name='Sushi' />
+        <FoodCategory name='Snacks' />
+        <FoodCategory name='Lunch' />
+      </View>
+      <View style={styles.row}>
+        <FoodCategory name='Pizza' />
+        <FoodCategory name='Sushi' />
+        <FoodCategory name='Snacks' />
+        <FoodCategory name='Lunch' />
+      </View>
+      <View
+        style={{
+          width: '100%',
+          paddingHorizontal: 30,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{ fontSize: 20, fontWeight: 'bold', alignSelf: 'flex-start' }}
+        >
+          Nhà hàng mới
+        </Text>
+      </View>
+      <View style={styles.card}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginVertical: 20,
-    backgroundColor: '#fd7a5c',
-    borderRadius: 5,
-  },
-  text: {
-    color: '#fff',
-    fontSize: 16,
-    textTransform: 'uppercase',
-    fontWeight: '600',
-  },
-});
+const FoodCategory = ({ name }) => {
+  const { colors } = useTheme();
+  const styles = getStyles(colors);
+  return (
+    <TouchableNativeFeedback>
+      <View style={styles.foodBtn}>
+        <View style={styles.foodBtnImage}></View>
+        <Text style={{ marginTop: 5 }}>{name}</Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
+};
+
+const getStyles = colors =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    header: {
+      width: '100%',
+      padding: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    banner: {
+      width: '100%',
+      height: 200,
+      backgroundColor: '#ccc',
+      marginBottom: 40,
+    },
+    search: {
+      position: 'absolute',
+      width: 'auto',
+      top: 170,
+      left: 30,
+      right: 30,
+    },
+    searchBox: {
+      backgroundColor: colors.white,
+      fontSize: 15,
+    },
+    searchBtn: {
+      position: 'absolute',
+      top: 14,
+      right: 10,
+    },
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    foodBtn: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      margin: 10,
+    },
+    foodBtnImage: {
+      width: 60,
+      height: 60,
+      borderRadius: 5,
+      backgroundColor: '#ccc',
+    },
+    card: {
+      flex: 1,
+      height: 100,
+      backgroundColor: colors.card,
+      marginHorizontal: 30,
+    },
+  });
